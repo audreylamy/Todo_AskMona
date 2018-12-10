b8afb02c073cb4ee4643bea01d3be9dac60c2fe5
Audrey LAMY <alamy@e1r7p2.42.fr>
2018-12-10 10:36:20 +0100

    body todo/ find all tasks api

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class TodoList extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            allTasks: null
+          }
+    }
+
+    async componentDidMount() {
+        const allTasks = await axios.get("https://todo-test-mona.herokuapp.com/tasks")
+        this.setState({
+            allTasks: allTasks.data
+        })
+    }
+
+    rendertasks() {
+        console.log(this.state.allTasks)
+        if (this.state.allTasks != null) {
+            return this.state.allTasks.map((tasks, i) => {
+              return (
+                <div key={i} className="todo_body">
+                    <input type="checkbox" id="horns" name="horns"/>
+                    <div className="App">
+                        {tasks.name}
+                    </div>
+                </div>
+              );
+            })
+          } else {
+            return (
+              <div className="App">
+                not
+              </div>
+            );
+          }
     }
 
     render() {
@@ -12,9 +46,7 @@ class TodoList extends Component {
                 <div className="todo_header">
                     <h3>My Todo List</h3>
                 </div>
-                <div className="todo_body">
-                
-                </div>
+                {this.rendertasks()}
                 <div className="todo_footer">
    
                 </div>
