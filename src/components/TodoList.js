bd824590baa6ef54e464809f4dd41fcc707cc8a4
Audrey LAMY <alamy@e1r7p2.42.fr>
2018-12-10 10:28:19 +0100

    header todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 
 class TodoList extends Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="todo">
-               hello
+                <div className="todo_header">
+                    <h3>My Todo List</h3>
+                </div>
+                <div className="todo_body">
+                
+                </div>
+                <div className="todo_footer">
+   
+                </div>
             </div>
         );
     }
