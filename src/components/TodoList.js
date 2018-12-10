8de34c70f59e174ab77e79a0614685a995768594
Audrey LAMY <alamy@e1r7p2.42.fr>
2018-12-10 10:20:55 +0100

    add landing

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+
+    render() {
+        return (
+            <div className="todo">
+               hello
+            </div>
+        );
+    }
+}
+
+export default TodoList;
